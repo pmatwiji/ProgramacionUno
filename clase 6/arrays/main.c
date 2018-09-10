1d997c24491960f20f65aa2450ee8d109a19f4ce
pmatwiji <pmatwiji@gmail.com>
2018-09-10 12:23:12 -0300

    clase 6

---
+++ clase 6/arrays/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializarArray (int parametroArray[], int cantidad, int valorInicio);
+void mostrarVector (arrayNuevo[cantidad], cantidad);
+
+
+int main()
+{
+
+    int cantidad = 5;
+    int arrayNuevo[cantidad];
+    int valorInicio = -1;
+    //funciones
+    inicializarArray(arrayNuevo[cantidad], cantidad, valorInicio);
+    void mostrarVector (arrayNuevo[cantidad], cantidad)
+    return 0;
+}
+
+void inicializarArray (int arrayNuevo[], int cantidad, int valorinicial)
+{
+    for(; cantidad>0; cantidad--)
+    {
+        arrayNuevo[cantidad]=valorinicial;
+    }
+}
+
+
+void mostrarVector (arrayNuevo[cantidad], cantidad)
+{
+    for(; cantidad>0; cantidad--)
+    {
+        printf("%d", arrayNuevo[cantidad]);
+    }
+}
+
+
+
---
+++ clase 6/cadenas/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char cadena [20]= "perro"; // = {'h','o','l','a'}; //"hola"
+    char cadena2 [20] = "gato";
+    int cantidad;
+    int estado;
+    /* cantidad = strlen(cadena); //devuelve int con cantidad de chars
+    printf("%d", cantidad);*/
+
+    /*strcpy(cadena, "gato");
+    // primer valor es donde lo va a copiar, segundo es lo que copia
+    printf("%s", cadena);*/
+
+
+    //compara palabras
+    // 0 son iguales
+    // -1 o 1 son distitntos
+    //estado = strcmp(cadena, "perro");
+
+    strupr(cadena); //to upper
+    //strlwr(cadena); //to lower
+    //puts(cadena);  //solo por referencia, sin editar dentro
+    //printf("%d", estado);
+
+    strcat(cadena2, cadena); //al primero le concateno el segundo
+    puts(cadena2);
+
+    return 0;
+}
+
+/*
+    printf("ingrese texto: ");
+    fflush(stdin);
+    //scanf("%s", &cadena);
+    gets(cadena);
+    printf("%s", cadena);
+*/
+
+
+
+/*
+//cadenas
+nombre 20
+apellido 20
+apellido y nombre 40
+
+nombre: juan carlos
+apellido gomez
+apellido y nombre gomez, juan carlos
+
+
+
+
+
+
+
+
+*/
