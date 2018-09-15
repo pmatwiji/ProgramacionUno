339b6eb3f52d35a719e96175bd7273018416a247
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-09-15 12:30:55 -0300

    carpetas ordenadas

--- tps/tp_laboratio_1/tp1/README.md
+++
@@ -1,1 +0,0 @@
-# tpLaboratorioUno
--- tps/tp_laboratio_1/tp1/biblioteca.h
+++
@@ -1,9 +0,0 @@
-/** \brief pide un entero y lo retorna
- *
- * \param char[] mensaje para el usuario
- * \return int el numero ingresado
- *
- */
-
-
-int pedirEntero(char[]);
