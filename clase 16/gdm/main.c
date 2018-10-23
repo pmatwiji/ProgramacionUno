fb58543254de84240e69ece3ad23821bb9392820
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-10-23 12:31:56 -0300

    clase 16.2

--- clase 16/gdm/main.c
+++ clase 16/gdm/main.c
@@ -4,12 +4,12 @@
 int main()
 {
     int* pEntero;
-    pEntero = (int*) malloc(sizeof(int)*5); /**malloc(sizeof(int)*5) crea un array en heap**/
+    int i;
+    pEntero = (int*) malloc(sizeof(int)*5); /** malloc(sizeof(int)*5) crea un array en heap **/
     if( pEntero != NULL)
     {
 //        *pEntero = 9;
 //        printf("%d", *pEntero);
-    int i;
         for(i=0; i < 5; i++)
         {
             printf("Ingrese un numero: \n");
@@ -20,7 +20,22 @@ int main()
         {
             printf("%d--", *(pEntero+i));
         }
-    pEntero = realloc(pEntero, sizeof(int)*10);
+        pEntero = realloc(pEntero, sizeof(int)*10);
+
+        printf("10 Datos");
+        if( pEntero != NULL)
+        {
+            for(i=5; i < 10; i++)
+            {
+                printf("Ingrese un numero: \n");
+                scanf("%d", pEntero+i);
+            }
+
+            for(i=0; i < 10; i++)
+            {
+                printf("%d--", *(pEntero+i));
+            }
+        }
 
     }
 
