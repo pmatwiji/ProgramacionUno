30502b86c2fbf9b55816b4683ba28f9f47d189fe
Pablo Matwijiszyn <pmatwiji@gmail.com>
2019-05-20 20:07:27 -0300

    maximo

--- repaso_punteros/main.c
+++ repaso_punteros/main.c
@@ -3,6 +3,8 @@
 
 int cargarVector(int*, int,char[]);
 int mostrarVector(int*, int);
+int buscarMaximo(int*, int, int*);
+int mostrarMaximo (int*, int,int*);
 
 int main()
 {
@@ -10,6 +12,7 @@ int main()
     int* pVector;
     int opcion;
     int validacion;
+    int maximo;
 
     pVector = vector;
     do
@@ -17,7 +20,8 @@ int main()
         printf("Menu:\n"
                "1. Cargar vector\n"
                "2. Mostrar vector\n"
-               "3. Salir\n"
+               "3. Maximo\n"
+               "4. Salir\n"
                "Ingrese una opcion: ");
         scanf("%d", &opcion);
         switch(opcion)
@@ -32,14 +36,27 @@ int main()
             system("cls");
             break;
         case 2:
-            mostrarVector(pVector,5);
+            validacion = mostrarVector(pVector,5);
+            if(validacion == 0)
+                printf("Datos cargados con exito\n");
+            else
+                printf("Error.");
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            validacion = mostrarMaximo(pVector, 5, &maximo);
+            if(validacion == 0)
+                printf("Datos cargados con exito\n");
+            else
+                printf("Error.");
             system("pause");
             system("cls");
             break;
         }
 
     }
-    while(opcion!=3);
+    while(opcion!=4);
 
 
 
@@ -82,9 +99,54 @@ int mostrarVector(int* pVector, int largo)
     }
     else
     {
-        printf("Error. No se pueden cargar los datos");
+        printf("Error. No se pueden cargar los datos\n");
     }
     return retorno;
-
-
 }
+
+
+int buscarMaximo(int* pVector, int largo, int* maximo)
+{
+    int retorno = 1;
+    int i;
+    int auxMaximo = 0;
+
+    if(pVector != NULL)
+    {
+        for(i=0; i<largo; i++)
+        {
+            if(*(pVector+i) > auxMaximo)
+            {
+                auxMaximo = *(pVector+i);
+                *maximo = auxMaximo;
+            }
+
+        }
+        retorno = 0;
+    }
+    else
+    {
+        printf("Error. No se pudo buscar el maximo.\n");
+    }
+
+    return retorno;
+}
+
+int mostrarMaximo (int* pVector, int largo,int* maximo)
+{
+    int retorno = 1;
+    int validacion = buscarMaximo(pVector, largo, maximo);
+    if(validacion!= 1 && pVector != NULL)
+    {
+        printf("El maximo es: %d\n", *maximo);
+        retorno = 0;
+
+    }
+    else
+    {
+        printf("Error. No se pudo mostrar el maximo");
+    }
+    return retorno;
+}
+
+
